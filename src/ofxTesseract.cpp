7e964ddac68b4eb51e1881795261b16204aac86f
Kyle McDonald <kyle@kylemcdonald.net>
2011-03-14 18:35:15 -0400

    first commit, includes libtesseract_full_i386.a compiled by rtwomey

---
+++ src/ofxTesseract.cpp
@@ -0,0 +1,46 @@
+#include "ofxTesseract.h"
+
+void ofxTesseract::setup(string dataPath, bool absolute, string language) {
+	// this has to be done in setup() instead of a constructor
+	// because the OF data path isn't ready during construction
+	string absoluteTessdataPath = absolute ? dataPath : ofToDataPath(dataPath, true);
+	
+	// there is some kind of bug in Init that ignores the datapath argument
+	// so we override it by setting an environment variable
+	setenv("TESSDATA_PREFIX", absoluteTessdataPath.c_str(), 1);
+	tess.Init(absoluteTessdataPath.c_str(), language.c_str());
+	setMode(AUTO);
+}
+
+void ofxTesseract::setWhitelist(string whitelistCharacters) {
+	tess.SetVariable("tessedit_char_whitelist", whitelistCharacters.c_str());
+}
+
+void ofxTesseract::setBlacklist(string blacklistCharacters) {
+	tess.SetVariable("tessedit_char_blacklist", blacklistCharacters.c_str());
+}
+
+void ofxTesseract::setMode(ofxTesseractMode mode) {
+	tess.SetPageSegMode((tesseract::PageSegMode) mode);
+}
+
+void ofxTesseract::setAccuracy(ofxTesseractAccuracy accuracy) {
+	tess.SetAccuracyVSpeed((tesseract::AccuracyVSpeed) accuracy);
+}
+
+string ofxTesseract::findText(ofImage& img) {
+	ofRectangle roi(0, 0, img.getWidth(), img.getHeight());
+	return findText(img, roi);
+}
+
+string ofxTesseract::findText(ofImage& img, ofRectangle& roi) {
+	ofPixels& pixels = img.getPixelsRef();
+	int bytesPerPixel = pixels.getBytesPerPixel();
+	return tess.TesseractRect(
+		pixels.getPixels(),
+		bytesPerPixel,
+		pixels.getWidth() * bytesPerPixel,
+		roi.x, roi.y,
+		roi.width, roi.height
+	);
+}
---
+++ src/ofxTesseract.h
@@ -0,0 +1,55 @@
+#pragma once
+
+/*
+	ofxTesseract by @kcimc
+	with help from rtwomey and julapy
+	based on this discussion
+	http://www.openframeworks.cc/forum/viewtopic.php?f=10&t=3728&p=27402
+*/
+
+#include "baseapi.h"
+#include "ofMain.h"
+
+class ofxTesseract {
+public:
+	enum ofxTesseractMode {
+		AUTO = tesseract::PSM_AUTO,
+		COLUMN = tesseract::PSM_SINGLE_COLUMN,
+		BLOCK = tesseract::PSM_SINGLE_BLOCK,
+		LINE = tesseract::PSM_SINGLE_LINE,
+		WORD = tesseract::PSM_SINGLE_WORD,
+		CHAR = tesseract::PSM_SINGLE_CHAR
+	};
+	
+	enum ofxTesseractAccuracy  {
+		ACCURATE = tesseract::AVS_MOST_ACCURATE,
+		FAST = tesseract::AVS_FASTEST
+	};
+
+	// dataPath is where to find a folder with tessdata inside
+	// default arguments assume bin/data/tessdata/eng.traineddata
+	void setup(string dataPath = "", bool absolute = false, string language = "eng");
+	
+	// you must call these after setup, contrary to the tesseract-ocr docs
+	// whitelist means only those characters are used
+	void setWhitelist(string whitelistCharacters);
+	// blacklist means none of those characters are used
+	void setBlacklist(string blacklistCharacters);
+	
+	// provides a hint for how to segment things
+	// by default this is ofxTesseract::AUTO
+	void setMode(ofxTesseractMode mode);
+	
+	// either ofxTesseract::ACCURATE or ofxTesseract::FAST
+	// by default it is ofxTesseract::FAST
+	void setAccuracy(ofxTesseractAccuracy accuracy);
+		
+	// finally, do OCR on an image, or on an image within an roi
+	// make sure your text is at least 10 pixels tall
+	// OF_IMAGE_COLOR and OF_IMAGE_GRAYSCALE are both fine
+	string findText(ofImage& img);
+	string findText(ofImage& img, ofRectangle& roi);
+	
+protected:
+	tesseract::TessBaseAPI tess;
+};
