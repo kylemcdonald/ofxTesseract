92eaf44ae8a49561487f15f7f9b723e07ce80758
Kyle McDonald <kyle@kylemcdonald.net>
2012-04-19 16:21:29 -0400

    fixed for 0071

--- example/src/ofxAutoControlPanel.h
+++ example/src/ofxAutoControlPanel.h
@@ -16,12 +16,12 @@ class ofxAutoControlPanel : public ofxControlPanel {
 public:
 	ofxAutoControlPanel() {
 		setXMLFilename("settings.xml");
-		ofAddListener(ofEvents.update, this, &ofxAutoControlPanel::update);
-		ofAddListener(ofEvents.draw, this, &ofxAutoControlPanel::draw);
-		ofAddListener(ofEvents.keyPressed, this, &ofxAutoControlPanel::keyPressed);
-		ofAddListener(ofEvents.mousePressed, this, &ofxAutoControlPanel::mousePressed);
-		ofAddListener(ofEvents.mouseReleased, this, &ofxAutoControlPanel::mouseReleased);
-		ofAddListener(ofEvents.mouseDragged, this, &ofxAutoControlPanel::mouseDragged);
+		ofAddListener(ofEvents().update, this, &ofxAutoControlPanel::update);
+		ofAddListener(ofEvents().draw, this, &ofxAutoControlPanel::draw);
+		ofAddListener(ofEvents().keyPressed, this, &ofxAutoControlPanel::keyPressed);
+		ofAddListener(ofEvents().mousePressed, this, &ofxAutoControlPanel::mousePressed);
+		ofAddListener(ofEvents().mouseReleased, this, &ofxAutoControlPanel::mouseReleased);
+		ofAddListener(ofEvents().mouseDragged, this, &ofxAutoControlPanel::mouseDragged);
 	}
 	void update(ofEventArgs& event) {
 		ofxControlPanel::update();
